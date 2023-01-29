6d5d5959c6344bb96d465e44a7812af6e95fb574
Grachic <Max2827@yandex.ru>
2023-01-29 21:02:29 +0300

    RestApi lib

--- Async_RestAPI/MainWindow.xaml.cs
+++ Async_RestAPI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,108 @@ namespace Async_RestAPI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private List<string> PrepareLoadSites()
+        {
+            List<string> sites = new List<string>()
+            {
+                "https://google.com",
+                "https://my.progtime.net"
+            };
+
+            return sites;
+        }
+
+        private void PrintInfo(DataModel dataModel)
+        {
+            textBlockInfo.Text += $"\nUrl: {dataModel.Url}, Length: {dataModel.Data.Length}";
+        }
+
+        public DataModel LoadSite(string site)
+        {
+            DataModel dataModel = new DataModel();
+
+            dataModel.Url = site;
+
+            WebClient webClient = new WebClient();
+            dataModel.Data = webClient.DownloadString(site);
+            
+            // обновление дизайна из другого потока
+            Dispatcher.BeginInvoke((Action) (() => {
+                textBlockInfo.Text = "Downloaded";
+            }));
+
+            return dataModel;
+        }
+        
+        
+
+        private void ButtonSync_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            LoadDataSync();
+
+            var time = stopwatch.ElapsedMilliseconds;
+
+            textBlockInfo.Text += $"\n\nTotal time: {time}";
+        }
+
+        public void LoadDataSync()
+        {
+            List<string> sites = PrepareLoadSites();
+
+            foreach (var site in sites)
+            {
+                var dataModel = LoadSite(site);
+                PrintInfo(dataModel);
+            }
+        }
+
+
+        private async void ButtonAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            //await LoadDataAsync();
+            await LoadDataAsyncParallel();
+
+            var time = stopwatch.ElapsedMilliseconds;
+
+            textBlockInfo.Text += $"\n\nTotal time: {time}";
+        }
+
+        public async Task LoadDataAsync()
+        {
+            List<string> sites = PrepareLoadSites();
+
+            foreach (var site in sites)
+            {
+                DataModel dataModel = await Task.Run(() => LoadSite(site));  // lock
+                PrintInfo(dataModel);
+            }
+        }
+
+        public async Task LoadDataAsyncParallel()
+        {
+            List<string> sites = PrepareLoadSites();
+
+            List<Task<DataModel>> tasks = new List<Task<DataModel>>();
+
+            foreach (var site in sites)
+            {
+                tasks.Add(Task.Run(() => LoadSite(site)));
+            }
+
+            DataModel[] dataModels =  await Task.WhenAll(tasks);
+
+            foreach (var dataModel in dataModels)
+            {
+                PrintInfo(dataModel);
+            }
+
+            textBlockInfo.Text = "";
         }
     }
 }
